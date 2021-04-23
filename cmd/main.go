7c26797dbee975ee9eb772a7a1403559fa775d19
nartikov <nartikov@gmail.com>
2021-04-23 16:20:58 +0300

    get config

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	handler "github.com/nartikov/kserver/pkg/handler"
 	"github.com/nartikov/kserver/pkg/repository"
 	"github.com/nartikov/kserver/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main()  {
+	if err:=initConfig(); err !=nil{
+		log.Fatalf("error init configs: %s", err.Error())
+	}
+
+
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv:=new(kser.Server)
-	if err:=srv.Run("8080",handlers.InitRoutes()); err!=nil{
+	if err:=srv.Run(viper.GetString("port"),handlers.InitRoutes()); err!=nil{
 		log.Fatalf("run error: %s", err.Error())
 	}
+}
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
